eb5c849c4e874d4c463db912c40b9c8a4e51a9cc
Joshua Peek <josh@users.noreply.github.com>
2025-03-18 19:50:40 -0700

    Init session

--- trakt_data.py
+++ trakt_data.py
@@ -1,6 +1,7 @@
 import logging
 
 import click
+import requests
 
 logger = logging.getLogger("trakt-data")
 
@@ -29,7 +30,26 @@ def main(
 ) -> None:
     logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
 
-    print("Hello, World!")
+    _session = _trakt_session(
+        client_id=trakt_client_id,
+        access_token=trakt_access_token,
+    )
+
+
+_TRAKT_API_HEADERS = {
+    "Content-Type": "application/json",
+    "trakt-api-key": "",
+    "trakt-api-version": "2",
+    "Authorization": "Bearer [access_token]",
+}
+
+
+def _trakt_session(client_id: str, access_token: str) -> requests.Session:
+    session = requests.Session()
+    session.headers.update(_TRAKT_API_HEADERS)
+    session.headers["trakt-api-key"] = client_id
+    session.headers["Authorization"] = f"Bearer {access_token}"
+    return session
 
 
 if __name__ == "__main__":
