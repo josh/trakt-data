bacd87ca6dfc7118b9ae156aa3e404cf0074bf63
Joshua Peek <josh@users.noreply.github.com>
2025-03-19 16:18:17 -0700

    Export hidden items

--- trakt_data.py
+++ trakt_data.py
@@ -44,71 +44,41 @@ class ExportUserProfile(TypedDict):
     vip_years: int
 
 
-class ExportUserMoviesStats(TypedDict):
-    plays: int
-    watched: int
-    minutes: int
-    collected: int
-    ratings: int
-    comments: int
+class ShowIds(TypedDict):
+    trakt: int
+    slug: str
+    tvdb: int
+    imdb: str
+    tmdb: int
+    tvrage: int | None
 
 
-class ExportUserShowsStats(TypedDict):
-    watched: int
-    collected: int
-    ratings: int
-    comments: int
+class Show(TypedDict):
+    title: str
+    year: int
+    ids: ShowIds
 
 
-class ExportUserSeasonsStats(TypedDict):
-    ratings: int
-    comments: int
+class MovieIds(TypedDict):
+    trakt: int
+    slug: str
+    imdb: str
+    tmdb: int
 
 
-class ExportUserEpisodesStats(TypedDict):
-    plays: int
-    watched: int
-    minutes: int
-    collected: int
-    ratings: int
-    comments: int
+class Movie(TypedDict):
+    title: str
+    year: int
+    ids: MovieIds
 
 
-class ExportUserNetworkStats(TypedDict):
-    friends: int
-    followers: int
-    following: int
+class Context:
+    session: requests.Session
+    output_dir: Path
 
-
-ExportUserRatingsDistribution = TypedDict(
-    "ExportUserRatingsDistribution",
-    {
-        "1": int,
-        "2": int,
-        "3": int,
-        "4": int,
-        "5": int,
-        "6": int,
-        "7": int,
-        "8": int,
-        "9": int,
-        "10": int,
-    },
-)
-
-
-class ExportUserRatingsStats(TypedDict):
-    total: int
-    distribution: ExportUserRatingsDistribution
-
-
-class ExportUserStats(TypedDict):
-    movies: ExportUserMoviesStats
-    shows: ExportUserShowsStats
-    seasons: ExportUserSeasonsStats
-    episodes: ExportUserEpisodesStats
-    network: ExportUserNetworkStats
-    ratings: ExportUserRatingsStats
+    def __init__(self, session: requests.Session, output_dir: Path) -> None:
+        self.session = session
+        self.output_dir = output_dir
 
 
 def _write_json(path: Path, obj: Any) -> None:
@@ -125,32 +95,32 @@ def _trakt_session(client_id: str, access_token: str) -> requests.Session:
     return session
 
 
-def _read_json_mtime_data(
-    path: Path,
-    return_type: type[T],
-) -> tuple[T, datetime] | tuple[None, None]:
+def _fresh(path: Path, max_age: timedelta) -> bool:
     if not path.exists():
-        return None, None
-    mtime = datetime.fromtimestamp(path.stat().st_mtime)
-    obj = json.loads(path.read_text())
-    return cast(T, obj), mtime
+        logger.debug("%s doesn't exist", path)
+        return False
+    else:
+        mtime = datetime.fromtimestamp(path.stat().st_mtime)
+        if mtime > datetime.now() - max_age:
+            logger.debug("%s last modified %s, still fresh", path, mtime)
+            return True
+        return False
 
 
-def _export_user_profile(
-    session: requests.Session,
-    output_path: Path,
-) -> ExportUserProfile:
-    user_profile, mtime = _read_json_mtime_data(output_path, ExportUserProfile)
+def _export_user_profile(ctx: Context) -> None:
+    output_path = ctx.output_dir / "user" / "profile.json"
 
-    if mtime and user_profile and mtime > datetime.now() - timedelta(days=7):
-        logger.debug("%s mtime is %s, still fresh", output_path, mtime)
-        return user_profile
+    if _fresh(output_path, timedelta(days=7)):
+        return
 
-    response = session.get("https://api.trakt.tv/users/me", params={"extended": "vip"})
+    response = ctx.session.get(
+        "https://api.trakt.tv/users/me",
+        params={"extended": "vip"},
+    )
     response.raise_for_status()
     data = response.json()
 
-    return {
+    profile = {
         "username": data["username"],
         "name": data["name"],
         "vip": data["vip"],
@@ -159,30 +129,81 @@ def _export_user_profile(
         "vip_og": data["vip_og"],
         "vip_years": data["vip_years"],
     }
+    _write_json(output_path, profile)
 
 
-def _export_user_stats(
-    session: requests.Session,
-    output_path: Path,
-) -> ExportUserStats:
-    user_stats, mtime = _read_json_mtime_data(output_path, ExportUserStats)
+def _export_user_stats(ctx: Context) -> None:
+    output_path = ctx.output_dir / "user" / "stats.json"
 
-    if mtime and user_stats and mtime > datetime.now() - timedelta(days=1):
-        logger.debug("%s mtime is %s, still fresh", output_path, mtime)
-        return user_stats
+    if _fresh(output_path, timedelta(days=1)):
+        return
 
-    response = session.get("https://api.trakt.tv/users/me/stats")
+    response = ctx.session.get("https://api.trakt.tv/users/me/stats")
     response.raise_for_status()
     data = response.json()
+    _write_json(output_path, data)
 
-    return {
-        "movies": data["movies"],
-        "shows": data["shows"],
-        "seasons": data["seasons"],
-        "episodes": data["episodes"],
-        "network": data["network"],
-        "ratings": data["ratings"],
-    }
+
+def _export_hidden(
+    ctx: Context,
+    section: str,
+    filename: str,
+    expires_in: timedelta,
+) -> None:
+    output_path = ctx.output_dir / "hidden" / filename
+
+    if _fresh(output_path, expires_in):
+        return
+
+    response = ctx.session.get(section)
+    response.raise_for_status()
+    data = response.json()
+    _write_json(output_path, data)
+
+
+def _export_hidden_calendar(ctx: Context) -> None:
+    _export_hidden(
+        ctx,
+        section="calendar",
+        filename="hidden-calendar.json",
+        expires_in=timedelta(days=1),
+    )
+
+
+def _export_hidden_progress_collected(ctx: Context) -> None:
+    _export_hidden(
+        ctx,
+        section="progress_collected",
+        filename="hidden-progress-collected.json",
+        expires_in=timedelta(days=7),
+    )
+
+
+def _export_hidden_progress_watched_reset(ctx: Context) -> None:
+    _export_hidden(
+        ctx,
+        section="progress_watched_reset",
+        filename="hidden-progress-watched-reset.json",
+        expires_in=timedelta(days=1),
+    )
+
+
+def _export_hidden_progress_watched(ctx: Context) -> None:
+    _export_hidden(
+        ctx,
+        section="progress_watched",
+        filename="hidden-progress-watched.json",
+        expires_in=timedelta(days=1),
+    )
+
+
+def _export_hidden_recommendations(ctx: Context) -> None:
+    _export_hidden(
+        ctx,
+        section="recommendations",
+        filename="hidden-recommendations.json",
+        expires_in=timedelta(days=7),
+    )
 
 
 def _read_json_data(path: Path, return_type: type[T]) -> T:
@@ -238,13 +259,18 @@ def main(
         access_token=trakt_access_token,
     )
 
-    user_profile_path = output_dir / "user" / "profile.json"
-    profile = _export_user_profile(_session, output_path=user_profile_path)
-    _write_json(user_profile_path, profile)
+    ctx = Context(
+        session=_session,
+        output_dir=output_dir,
+    )
 
-    user_stats_path = output_dir / "user" / "stats.json"
-    stats = _export_user_stats(_session, output_path=user_stats_path)
-    _write_json(user_stats_path, stats)
+    _export_hidden_calendar(ctx)
+    _export_hidden_progress_collected(ctx)
+    _export_hidden_progress_watched_reset(ctx)
+    _export_hidden_progress_watched(ctx)
+    _export_hidden_recommendations(ctx)
+    _export_user_profile(ctx)
+    _export_user_stats(ctx)
 
     _generate_metrics(data_path=output_dir)
 
