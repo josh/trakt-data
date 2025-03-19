5904c58c544be589f45e7e2f0adbd1f7240821df
Joshua Peek <josh@users.noreply.github.com>
2025-03-18 19:49:28 -0700

    Define logger

--- trakt_data.py
+++ trakt_data.py
@@ -1,4 +1,8 @@
+import logging
+
 import click
+
+logger = logging.getLogger("trakt-data")
 
 
 @click.command()
@@ -12,10 +16,19 @@ import click
     required=True,
     envvar="TRAKT_ACCESS_TOKEN",
 )
+@click.option(
+    "--verbose",
+    "-v",
+    is_flag=True,
+    help="Enable verbose logging",
+)
 def main(
     trakt_client_id: str,
     trakt_access_token: str,
+    verbose: bool,
 ) -> None:
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+
     print("Hello, World!")
 
 
