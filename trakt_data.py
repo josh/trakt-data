d0bf4044002cc9631a5bff16f3dae21e752f0010
Joshua Peek <josh@users.noreply.github.com>
2025-03-18 20:04:44 -0700

    Fetch some basic profile info

--- trakt_data.py
+++ trakt_data.py
@@ -1,4 +1,5 @@
 import logging
+from typing import TypedDict
 
 import click
 import requests
@@ -35,6 +36,10 @@ def main(
         access_token=trakt_access_token,
     )
 
+    profile = _export_user_profile(_session)
+    print("user/profile.json")
+    print(profile)
+
 
 _TRAKT_API_HEADERS = {
     "Content-Type": "application/json",
@@ -50,6 +55,36 @@ def _trakt_session(client_id: str, access_token: str) -> requests.Session:
     session.headers["trakt-api-key"] = client_id
     session.headers["Authorization"] = f"Bearer {access_token}"
     return session
+
+
+class UserIDs(TypedDict):
+    slug: str
+
+
+class ExportUserProfile(TypedDict):
+    username: str
+    name: str
+    vip: bool
+    vip_ep: bool
+    ids: UserIDs
+    vip_og: bool
+    vip_years: int
+
+
+def _export_user_profile(session: requests.Session) -> ExportUserProfile:
+    response = session.get("https://api.trakt.tv/users/me", params={"extended": "vip"})
+    response.raise_for_status()
+    data = response.json()
+
+    return {
+        "username": data["username"],
+        "name": data["name"],
+        "vip": data["vip"],
+        "vip_ep": data["vip_ep"],
+        "ids": data["ids"],
+        "vip_og": data["vip_og"],
+        "vip_years": data["vip_years"],
+    }
 
 
 if __name__ == "__main__":
